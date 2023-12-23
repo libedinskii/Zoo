1ed525b430a463c4557cd8db281e9b6f7955f2d5
libedinskii <124511733+libedinskii@users.noreply.github.com>
2023-12-23 20:01:02 +0900

    Create Elephant.java

---
+++ Elephant.java
@@ -0,0 +1,10 @@
+public class Elephant extends Animal {
+    public Elephant(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Trumpet!");
+    }
+}
